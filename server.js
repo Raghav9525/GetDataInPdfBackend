989eb037e4cdb092736dd329021c38fe8f2221d0
Raghav9525 <kshubham9525.gmail.com>
2024-04-14 17:42:39 +0530

    route problem

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
-PORT = process.env.PORT || 5000
+const path = require('path'); // Require the path module
+const userDataRoutes = require('./routes/userData');
 
-const userDataRoutes = require('./routes/userData')
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
     origin: "*" // Allow all origins
 }));
 
-app.use('/',userDataRoutes);
+// Define your API routes before serving static files
+app.use('/', userDataRoutes);
 
+// Serve static files from the 'build' directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Route all requests to index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
