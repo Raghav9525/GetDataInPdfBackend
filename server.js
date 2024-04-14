98af0dc096995956eb2270a643e78fddb46a0a20
Raghav9525 <kshubham9525.gmail.com>
2024-04-14 17:05:37 +0530

    first

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+PORT = process.env.PORT || 5000
+
+const userDataRoutes = require('./routes/userData')
+
+app.use(express.json());
+app.use(cors({
+    origin: "*" // Allow all origins
+}));
+
+app.use('/',userDataRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
